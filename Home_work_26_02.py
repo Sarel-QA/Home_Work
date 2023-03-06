61e3cf3f793f17d6a495771078946e8019c847d5
Sarel_King <sarel@shekelbrainweigh.com>
2023-03-06 12:12:30 +0200

    Home_work

---
+++ Home_work_26_02.py
@@ -0,0 +1,18 @@
+##### 26/02/2023 ######
+# # 1)
+# user_enter_name = input('Enter your name:')
+# user_enter_age = int(input('Enter your age:'))
+# print('Hello my name is' + ' ' + user_enter_name, 'I am:' + ' ' + str(user_enter_age))
+#
+# # 2)
+# user_enter_basis = int(input('Enter basis:'))
+# user_enter_height = int(input('Enter height:'))
+# print('the area of a triangle is:', float(user_enter_basis * user_enter_height)/2)
+#
+# # 3)
+# x = 'hello world'
+# sub = x[6:11]
+# print(sub)
+
+##### 05/03/2023 ######
+
