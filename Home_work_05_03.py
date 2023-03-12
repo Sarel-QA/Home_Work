7b77935f6aa6f8a4034c49da8ae5e61883c4d477
Sarel <sarelking123@gmail.com>
2023-03-12 20:08:35 +0200

    Home work 05/03/23

--- Home_work_05_03.py
+++ Home_work_05_03.py
@@ -1,37 +1,40 @@
-                                                                                    ##### 05/03/2023 ######
+##### 05/03/2023 ######
 # 1)
-def last_and_first (name):
-    if name[0] == name[3]:
+def last_and_first(name):
+    if name[0] == name[-1]:
         return True
     else:
         return False
 
+
 letters = last_and_first('sail')
 print(f'the answer for exercise 1 is {letters}')
 
-# bonus#1
-# def last_and_first (name): return name[0] == name[3]
 
-# letters_2 = last_and_first('sars')
-# print(f'the answer for bonus exercise is {letters_2}')
+# bonus_1
+def last_and_first (name): return name[0] == name[-1]
 
-# bonus#2
-# def last_and_first (name): return name[0] == name[3]
-#
-# b = last_and_first('Sars')
-# print(f'the answer for bonus exercise is {b}')
+letters_2 = last_and_first('sars')
+print(f'the answer for bonus number 1 is {letters_2}')
+
+# bonus_2
+def last_and_first(name): return name[0].lower() == name[3].lower()
+
+b = last_and_first('Sars')
+print(f'the answer for bonus number 2 is {b}')
+
 
 # 2)
-def powers_that_be (low, high):
-    return low ** high
+def powers_that_be(num_1, num_2):
+    return num_1 ** num_2
+
+
 calculation = powers_that_be(4, 7)
 print(f'the answer for exercise 2 is {calculation}')
 
+
 # 3)
-def combine_lists (list1, list2):
-    return list1[2:3] and list2[0:1]
+def combine_lists(list1, list2):
+    return list1[:len(list1)//2] + list2[len(list2)//2:]
 
-list1 = ['a','b',6,5]
-list_2 = [True,'z', 1, 0]
-
-print(list1, list_2)
+print(combine_lists(['a', 'b', 6, 5] , [True, 'z', 1, 8]))
