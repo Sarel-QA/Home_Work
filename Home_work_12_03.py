31b085ecc61b48cc9bc4390610216af699c45fbf
Sarel <sarelking123@gmail.com>
2023-03-19 13:30:07 +0200

    Home work 12/03/23

---
+++ Home_work_12_03.py
@@ -0,0 +1,21 @@
+##### 05/03/2023 ######
+# 1)
+# def print_negative(number):
+#     i = int(input('enter number:'))
+#     while i <= number:
+#         print(i)
+#         i += 1
+#     else:
+#         print()
+# print_negative(0)
+
+#  2)
+def count_capital(s):
+    counter = 0
+    for c in s:
+        if c in 'abcABC':
+            counter += 1
+    return counter
+print(count_capital(input('please enter string:')))
+
+
