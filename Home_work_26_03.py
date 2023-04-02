21150aed6bc106ab74f2c1e4c89bc3fabee40008
Sarel <sarelking123@gmail.com>
2023-04-02 16:09:39 +0300

    Home work 26/03/23

---
+++ Home_work_26_03.py
@@ -0,0 +1,21 @@
+##### 26/03/2023 ######
+# 1)
+
+def letters_to_file(filename, string_to_file, list):
+    f = open(filename, 'w')
+
+    words = string_to_file.split()
+    for each_word in words:
+        for char in list:
+            if char in each_word:
+                print(each_word)
+                f.write(each_word + ' ')
+                break
+    f.close()
+
+
+
+letters_to_file('my_home_work_file.txt', 'hello to all the students', ['d', 'a', 'Z'])
+
+
+
